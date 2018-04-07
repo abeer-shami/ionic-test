6dae993a114658a7949363e8128d1fb9a36770b0
abeer shami <abeer.shami887@gmail.com>
2018-04-07 23:56:16 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpModule } from "@angular/http";
+
+import { AboutPage } from '../pages/about/about';
+import { SettingsPage } from '../pages/settings/settings';
+import { RedditsPage } from '../pages/reddits/reddits';
+import { TabsPage } from '../pages/tabs/tabs';
+import {DetailsPage} from '../pages/details/details';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    SettingsPage,
+    RedditsPage,
+    DetailsPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    SettingsPage,
+    RedditsPage,
+    DetailsPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
